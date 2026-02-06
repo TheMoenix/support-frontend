160a148aa1b523ec1bd944232fc4cb4c13b141ae
Mohammed Alshami <alshami@juleb.com>
2026-02-07 01:24:19 +0300

    Implement internationalization with i18next; add language and theme toggles

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,5 +1,7 @@
-import { Layout, Button, Space, Typography, Flex } from "antd";
+import { Layout, Button, Space, Typography, Flex, Switch, theme } from "antd";
 import { useNavigate } from "react-router-dom";
+import { Language, Theme } from "../common/constants";
+import { MoonOutlined, SunOutlined } from "@ant-design/icons";
 
 const { Header } = Layout;
 const { Text } = Typography;
@@ -7,17 +9,53 @@ const { Text } = Typography;
 export default function AppHeader() {
   const navigate = useNavigate();
 
+  const currentLanguage =
+    (localStorage.getItem("language") as Language) || Language.ENGLISH;
+  const currentTheme = (localStorage.getItem("theme") as Theme) || Theme.LIGHT;
+
+  const handleLanguageToggle = (checked: boolean) => {
+    const newLanguage = checked ? Language.ARABIC : Language.ENGLISH;
+    localStorage.setItem("language", newLanguage);
+    window.location.reload();
+  };
+
+  const handleThemeToggle = (checked: boolean) => {
+    const newTheme = checked ? Theme.DARK : Theme.LIGHT;
+    localStorage.setItem("theme", newTheme);
+    window.location.reload();
+  };
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
-    <Header>
+    <Header style={{ background: colorBgContainer }}>
       <Flex justify="space-between" align="center">
-        <Text style={{ color: "white", fontSize: "20px" }}>Support</Text>
+        <Space size="large">
+          <Text style={{ fontSize: "25px" }}>Support</Text>
+          <Space>
+            <Button type="link" onClick={() => navigate("/create")}>
+              Create Ticket
+            </Button>
+            <Button type="link" onClick={() => navigate("/list")}>
+              List Tickets
+            </Button>
+          </Space>
+        </Space>
         <Space>
-          <Button type="link" onClick={() => navigate("/create")}>
-            Create Ticket
-          </Button>
-          <Button type="link" onClick={() => navigate("/list")}>
-            List Tickets
-          </Button>
+          <Switch
+            checked={currentTheme === Theme.DARK}
+            onChange={handleThemeToggle}
+            checkedChildren={<MoonOutlined />}
+            unCheckedChildren={<SunOutlined />}
+          />
+          <Switch
+            checked={currentLanguage === Language.ARABIC}
+            onChange={handleLanguageToggle}
+            checkedChildren="AR"
+            unCheckedChildren="EN"
+          />
         </Space>
       </Flex>
     </Header>
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ConfigProvider } from "antd";
 import App from "./App";
 import "./assets/root.css";
+import "./locale/index";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: "#15514F",
-          colorSuccess: "#4ea64e",
-          colorWarning: "#c5871f",
-          colorError: "#9c3b3b",
-          colorLink: "#27918D",
-          colorInfo: "#27918D",
-
-          fontSize: 16,
-        },
-      }}
-    >
-      <App />
-    </ConfigProvider>
+    <App />
   </StrictMode>,
 );
