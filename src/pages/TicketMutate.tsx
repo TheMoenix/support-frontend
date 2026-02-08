15b5cc97b1e40394bd65147f66e804fcb9ff093d
Mohammed Alshami <msalshami420@gmail.com>
2026-02-08 05:27:35 +0300

    basic form

--- src/pages/TicketMutate.tsx
+++ src/pages/TicketMutate.tsx
@@ -1,21 +1,62 @@
-import { Flex, Typography } from "antd";
+import { Button, Card, Form, Input } from "antd";
 import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-
-const { Title } = Typography;
+import { useForm } from "antd/es/form/Form";
 
 function TicketMutate() {
   const ticketId = useParams().id;
   const { t } = useTranslation();
+  const [form] = useForm();
+
+  const onFinish = (values: any) => {
+    console.log(values);
+    form.resetFields();
+  };
 
   return (
-    <Flex justify="center" align="center">
-      <Title type="success">
-        {ticketId
-          ? `${t("editingTicket")}: ${ticketId}`
-          : t("creatingNewTicket")}
-      </Title>
-    </Flex>
+    <Card
+      title={
+        ticketId ? `${t("editingTicket")}: ${ticketId}` : t("creatingNewTicket")
+      }
+      style={{ margin: "14px" }}
+    >
+      <Form form={form} onFinish={onFinish}>
+        <Form.Item
+          name={"name"}
+          label="form.name"
+          rules={[{ required: true, message: "message.required" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name={"email"}
+          label="form.email"
+          rules={[
+            { required: true, message: "message.required" },
+            { type: "email", message: "message.email_error" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name={"subject"}
+          label="form.subject"
+          rules={[{ required: true, message: "message.required" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name={"message"}
+          label="form.message"
+          rules={[{ required: true, message: "message.required" }]}
+        >
+          <Input.TextArea />
+        </Form.Item>
+        <Form.Item>
+          <Button htmlType="submit"> {t("button.submit")}</Button>
+        </Form.Item>
+      </Form>
+    </Card>
   );
 }
 
